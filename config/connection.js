232580ce07a2cacf8b35b993b8551d4bb102ddfa
Kyle Allen <Kallen478@gmail.com>
2020-11-21 22:26:13 -0500

    added functions for accessing table data

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,24 @@
+var mysql = require("mysql");
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else {
+  connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "rootroot",
+  database: "burgers_db"
+});
+}
+
+connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
+
+// export connection for our ORM to use
 module.exports = connection;
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,11 +2,32 @@ var express = require("express");
 var burger = require("../models/burger.js");
 var router = express.Router();
 
+router.get("/", function (req, res) {
+        burger.selectAll(function (data) {
+            var hbsObject = {
+                burgers: data
+            };
+            console.log(hbsObject);
+            res.render("index", hbsObject);
+        });
+});
 
+router.post("/", function (req, res) {
+    burger.insertOne(["burger_name"], [req.body.name], function (data) {
+        res.redirect("/");
+    });
+});
 
+router.put("/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+    console.log("condition", condition);
 
-
-
-
+    burger.updateOne({ devoured: req.body.devoured }, condition, function (burger) {
+        if (result.changedRows === 0) {
+            return res.status(404).end();
+          }
+          res.status(200).end();
+    });
+});
 
 module.exports = router;
